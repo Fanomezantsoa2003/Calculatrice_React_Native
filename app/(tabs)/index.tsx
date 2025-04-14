0dfc8ee47521bf1dfd2d9b02999cdb701bcec35e
Fanomezantsoa2003 <tsoutsougamer@gmail.com>
2025-04-14 19:02:00 +0300

    kely sisa

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,10 +5,28 @@ import { View, TouchableOpacity, Text, FlatList, TextInput } from 'react-native'
 const chiffre = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 export default function HomeScreen() {
   const [inputValue, setInputValue] = useState('');
+  const operateur = ['+', '-', '*', '/'];
+  const [result, setResult] = useState('');
 
   const handlePress = (item) => {
     console.log(item);
-    setInputValue(item);
+    // setInputValue(item);
+    setInputValue((prev) => {
+      if (item === 'C') {
+        return prev.slice(0, -1);
+      }
+      return prev + item;
+    }
+    );
+    if (item === '=') {
+      try {
+        const evalResult = eval(inputValue);
+        setResult(evalResult);
+        setInputValue(evalResult.toString());
+      } catch (error) {
+        console.error('Error evaluating expression:', error);
+      }
+    }
   };
   return (
     <View style={{ alignItems: 'center', justifyContent: 'center', height: '100%', width: '100%' }}>
@@ -29,17 +47,23 @@ export default function HomeScreen() {
           />
       </View>
       <View style={{flexWrap:'wrap', flexDirection:'row', justifyContent:'space-between', width:'100%', padding:10}}>
-        <TouchableOpacity style={{backgroundColor:'gray', padding:20, borderRadius:10}} onPress={() => handlePress('C')}>
+        <TouchableOpacity style={{backgroundColor:'gray', padding:20, borderRadius:10}} onPress={() => handlePress('+')}>
           <Text style={{ fontSize: 20, color: 'white', marginTop: 20 }}>+</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={{backgroundColor:'gray', padding:20, borderRadius:10}} onPress={() => handlePress('C')}>
+        <TouchableOpacity style={{backgroundColor:'gray', padding:20, borderRadius:10}} onPress={() => handlePress('-')}>
           <Text style={{ fontSize: 20, color: 'white', marginTop: 20 }}>-</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={{backgroundColor:'gray', padding:20, borderRadius:10}} onPress={() => handlePress('C')}>
+        <TouchableOpacity style={{backgroundColor:'gray', padding:20, borderRadius:10}} onPress={() => handlePress('*')}>
           <Text style={{ fontSize: 20, color: 'white', marginTop: 20 }}>*</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={{backgroundColor:'gray', padding:20, borderRadius:10}} onPress={() => handlePress('C')}>
+        <TouchableOpacity style={{backgroundColor:'gray', padding:20, borderRadius:10}} onPress={() => handlePress('/')}>
           <Text style={{ fontSize: 20, color: 'white', marginTop: 20 }}>/</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{backgroundColor:'gray', padding:20, borderRadius:10}} onPress={() => handlePress('=')}>
+          <Text style={{ fontSize: 20, color: 'white', marginTop: 20 }}>=</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{backgroundColor:'gray', padding:20, borderRadius:10}} onPress={() => handlePress('C')}>
+          <Text style={{ fontSize: 20, color: 'white', marginTop: 20 }}>Effacer</Text>
         </TouchableOpacity>
       </View>
     </View>
